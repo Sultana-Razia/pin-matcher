4f1a0bfe96c38dd26313c465efb36a00ca2de059
Sultana Razia <sultanarazia483@gmail.com>
2022-04-21 11:33:14 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const clacInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            clacInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = clacInput.value;
+        const newNumber = previousNumber + number;
+        clacInput.value = newNumber;
+    }
+});
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    if (pin == typedNumbers) {
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else {
+        successMessage.style.display = 'none';
+        failError.style.display = 'block';
+    }
+}
